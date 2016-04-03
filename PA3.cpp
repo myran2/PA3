4cefff1a6886a97a962cc37186241363c7081604
myran2 <henrytgordon@gmail.com>
2016-04-03 00:36:04 -0400

    Added stack class

--- PA3.cpp
+++ PA3.cpp
@@ -1,11 +1,21 @@
-// PA3.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
+#include "PA3.h"
 
 int main()
 {
+    Stack stack(5);
+
+    stack.push("1");
+    stack.push("2");
+    stack.push("3");
+    stack.push("4");
+    stack.push("5");
+    stack.print();
+
+    stack.pop();
+    cout << endl;
+
+    stack.print();
+    
     return 0;
 }
 

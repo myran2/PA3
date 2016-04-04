a97bb936111e0ea7a4b5ec14873ce443dda2f6fa
myran2 <henrytgordon@gmail.com>
2016-04-04 13:05:53 -0400

    Some work on tokenization

--- PA3.h
+++ PA3.h
@@ -14,9 +14,10 @@ class Stack
     public:
         Stack(int size);
 
-        void push(string line);
+        void push(string line, bool allowDupes);
         string pop();
-        void print();
+        void print(bool newLine);
+        bool empty() { return top != -1; };
 
 };
 
@@ -36,7 +37,7 @@ Stack::Stack(int initSize)
 }
 
 // adds a new entry to the end of the stack
-void Stack::push(string line)
+void Stack::push(string line, bool allowDupes = true)
 {
     if (top == maxSize)
     {
@@ -45,6 +46,15 @@ void Stack::push(string line)
         exit(1);
     }
 
+    if (!allowDupes)
+    {
+        for (int i = 0; i <= top; i++)
+        {
+            if (data[i] == line)
+                return;
+        }
+    }
+    
     top++;
     data[top] = line;
 
@@ -56,8 +66,6 @@ void Stack::push(string line)
         data = new string[maxSize];
         for (int i = 0; i <= top; i++)
             data[i] = holdData[i];
-
-        //delete holdData;
     }
 }
 
@@ -74,7 +82,7 @@ string Stack::pop()
 }
 
 // prints the stack to console, 1 new line per entry
-void Stack::print()
+void Stack::print(bool newLine)
 {
     if (top == -1)
     {
@@ -83,7 +91,7 @@ void Stack::print()
     }
     
     for (int i = 0; i <= top; i++)
-        cout << data[i] << "\n";
+        cout << data[i] << (newLine ? "\n" : " ");
 }
 
 // loads a file from 'fileName' into the 'lines' stack
@@ -103,4 +111,67 @@ void loadFile(const string& fileName, Stack& lines)
         lines.push(line);
 
     file.close();
+}
+
+bool remove(string& line, const string& r)
+{
+    cout << "Removing " << r << " from " << line << endl;
+    bool found = false;
+    while (line.find(r) != string::npos)
+    {
+        found = true;
+        line.erase(line.find(r), r.length());
+        cout << line << endl;
+    }
+
+    return found;
+}
+
+void tokenize(Stack& lines)
+{
+    const string KEYWORDS[] = { "BEGIN", "END", "FOR" };
+    //const string OPERATORS[] = { "+", "*", "-", "/", "++", "=" };
+    const string OPERATORS[] = { "+", "*", "-", "/", "=" };
+    const string DELIMITERS[] = { ",", ";" };
+
+    Stack keywords = Stack(2);
+    Stack operators = Stack(2);
+    Stack delimitors = Stack(2);
+
+    while (!lines.empty())
+    {
+        string line = lines.pop();
+
+        if (line == "BEGIN" || line == "END")
+        {
+            keywords.push(line, false);
+            continue;
+        }
+        
+        // 5 is length of OPERATORS
+        for (int i = 0; i < 5; i++)
+        {
+            if (remove(line, OPERATORS[i]))
+                operators.push(OPERATORS[i], false);
+        }
+
+        // 2 is length of DELIMITERS
+        for (int i = 0; i < 2; i++)
+        {
+            if (remove(line, DELIMITERS[i]))
+                delimitors.push(DELIMITERS[i], false);
+        }
+    }
+
+    cout << "Keywords: ";
+    keywords.print(false);
+    cout << endl;
+
+    cout << "Operators: ";
+    operators.print(false);
+    cout << endl;
+
+    cout << "Delimiters: ";
+    delimitors.print(false);
+    cout << endl;
 }
